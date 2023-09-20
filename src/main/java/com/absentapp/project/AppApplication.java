63372010a0e429e54cae1f637d98a39eb4d44089
JLPimenta <joaoluizlopespimenta@gmail.com>
2023-09-19 23:31:10 -0300

    ✨ Implementa swagger

--- src/main/java/com/absentapp/project/AppApplication.java
+++ src/main/java/com/absentapp/project/AppApplication.java
@@ -1,9 +1,12 @@
 package com.absentapp.project;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "AbsentApp API", version = "1", description = "API da aplicação AbsentApp"))
 public class AppApplication {
 
 	public static void main(String[] args) {
